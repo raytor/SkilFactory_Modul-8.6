ff0f00a584f5990d411f75ed4183fbf21b4676ab
raytor <11767851+raytor@users.noreply.github.com>
2021-05-24 13:27:15 +0300

    Add files via upload

---
+++ menu.inc.php
@@ -0,0 +1,9 @@
+            <nav>
+                <ul>
+                    <li><a href="#"> Моя учеба </a></li>
+                    <li><a href="#"> Мой portfolio </a></li>
+                    <li><a href="#"> Мои статья </a></li>
+                    <li><a href="#"> Ссылки </a></li>
+                    <li><a href="#"> Контакты </a></li>
+                </ul>    
+            </nav>
